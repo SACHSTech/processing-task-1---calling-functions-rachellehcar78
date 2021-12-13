18624e8edc86dd9461fa03f53a54741ef6763ebb
rachellehcar78 <95876157+rachellehcar78@users.noreply.github.com>
2021-12-13 12:17:09 -0500

    Update Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -5,18 +5,18 @@ public class Sketch extends PApplet {
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
-   */
+   ***/
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 800);
   }
 
-  /** 
+  /**   
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(94, 218, 255);
   }
 
   /**
@@ -24,13 +24,51 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	// sample code, delete this stuff    
+    // Petal #1
+    stroke(87);
+    fill(255, 122, 235);
+    ellipse(200, 250, 100, 100);
+    
+    // Petal #2
+    stroke(87);
+    fill(255, 122, 235);
+    ellipse(200, 300, 100, 100);
+    
+    // Petal #3
+    stroke(87);
+    fill(255, 122, 235);
+    ellipse(250, 325, 100, 100);
+    
+    // Petal #4
+    stroke(87);
+    fill(255, 122, 235);
+    ellipse(305, 300, 100, 100);
+    
+    // Petal #5
+    stroke(87);
+    fill(255, 122, 235);
+    ellipse(305, 250, 100, 100);
+    
+    // Petal #6
+    stroke(87);
+    fill(255, 122, 235);
+    ellipse(275, 200, 100, 100);
+    
+    // Petal #7
+    stroke(87);
+    fill(255, 122, 235);
+    ellipse(225, 200, 100, 100);
+    
+	  // Middle Part
+    stroke(87);
+    fill(255, 238, 56);
+    ellipse(250, 260, 50, 50);
+    
+    // Stem
+    stroke(87);
+    fill(21, 191, 30);
+    rect(245, 375, 10, 300);
   }
-  
-  // define other methods down here.
+
 }
